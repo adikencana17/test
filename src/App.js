50681f9adb786a370dc70c19a8ab9562282a2c28
adikencana17 <103553495+adikencana17@users.noreply.github.com>
2022-04-13 12:47:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -204,14 +204,14 @@ function App() {
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         <s.SpacerSmall />
         <ResponsiveWrapper flex={1} style={{ padding: 24 }} test>
-          <s.Container flex={1} jc={"center"} ai={"center"}>
+          <s.Container flex={1} jc={"left"} ai={"left"}>
             <StyledImg alt={"example"} src={"/config/images/example.gif"} />
           </s.Container>
           <s.SpacerLarge />
           <s.Container
             flex={2}
-            jc={"center"}
-            ai={"center"}
+            jc={"left"}
+            ai={"left"}
             style={{
               backgroundColor: "var(--accent)",
               padding: 24,
