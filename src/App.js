c2f21a941b89d31a77b5772a73a165c44d6e5167
adikencana17 <103553495+adikencana17@users.noreply.github.com>
2022-04-14 10:48:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -388,7 +388,6 @@ function App() {
             We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
             successfully mint your NFT. We recommend that you don't lower the
             gas limit.
-          </s.TextDescription>
      );
 }
 
